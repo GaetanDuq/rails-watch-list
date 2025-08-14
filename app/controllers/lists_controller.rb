f4c2d4409da0014fdbc3d36fc81a12540a5429f7
cdGaetan Duquesnoy <gaetan.duquesnoy@gmail.com>
2025-08-14 17:29:37 +0900

    Watch list day 1 done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @bookmarks = Bookmark.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
